cd1c95fe9ce680dad6c06f027181f455dfece611
Robert Sefer <rsefer@gmail.com>
2024-06-19 12:53:20 -0500

    Tmp

--- scripts/display-test.swift
+++ scripts/display-test.swift
@@ -14,34 +14,37 @@
 
 import AppKit
 import Foundation
+import Cocoa
 
-struct Layout: Decodable {
-	let screens : Any
-	let appGroups : Any
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: RootKeys.self)
-		screens = try values.decode(Int.self, forKey: .screens)
-		appGroups = try values.decode(Int.self, forKey: .appGroups)
-	}
-}
+print(AccessibilityElement.getFrontWindowElement())
 
-let runningApplications = NSWorkspace.shared.runningApplications.filter{ $0.activationPolicy == .regular }
-
-let layoutsJSONPath = "\(ProcessInfo.processInfo.environment["DOTFILES_ROOT"] ?? "")/mac-scripts/layouts.json"
-var layouts : Any = []
-
-if FileManager.default.fileExists(atPath: layoutsJSONPath) {
-	do {
-		let data = try Data(contentsOf: URL(fileURLWithPath: layoutsJSONPath))
-		layouts = try JSONDecoder().decode(Layout.self, from: data)
-
-	} catch {
-		print("Error reading JSON file: \(error)")
-	}
-}
-print(layouts)
-
-// NSScreen.screens.forEach {
-// 	print($0.localizedName)
-// 	print($0.visibleFrame)
+// struct Layout: Decodable {
+// 	let screens : Any
+// 	let appGroups : Any
+// 	init(from decoder: Decoder) throws {
+// 		let values = try decoder.container(keyedBy: RootKeys.self)
+// 		screens = try values.decode(Int.self, forKey: .screens)
+// 		appGroups = try values.decode(Int.self, forKey: .appGroups)
+// 	}
 // }
+
+// let runningApplications = NSWorkspace.shared.runningApplications.filter{ $0.activationPolicy == .regular }
+
+// let layoutsJSONPath = "\(ProcessInfo.processInfo.environment["DOTFILES_ROOT"] ?? "")/mac-scripts/layouts.json"
+// var layouts : Any = []
+
+// if FileManager.default.fileExists(atPath: layoutsJSONPath) {
+// 	do {
+// 		let data = try Data(contentsOf: URL(fileURLWithPath: layoutsJSONPath))
+// 		layouts = try JSONDecoder().decode(Layout.self, from: data)
+
+// 	} catch {
+// 		print("Error reading JSON file: \(error)")
+// 	}
+// }
+// print(layouts)
+
+// // NSScreen.screens.forEach {
+// // 	print($0.localizedName)
+// // 	print($0.visibleFrame)
+// // }
