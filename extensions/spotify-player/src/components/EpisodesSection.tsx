0d46fd38286d19ce0d471da55bca1ca4066c4ba2
Robert Sefer <rsefer@gmail.com>
2025-10-28 09:19:50 -0500

    Parity

--- extensions/spotify-player/src/components/EpisodesSection.tsx
+++ extensions/spotify-player/src/components/EpisodesSection.tsx
@@ -11,11 +11,13 @@ type EpisodesSectionProps = {
 export function EpisodesSection({ episodes, limit, title = "Episodes" }: EpisodesSectionProps) {
   if (!episodes) return null;
 
-  const items = episodes.slice(0, limit || episodes.length).filter(item => item !== null);
+  const items = episodes.slice(0, limit || episodes.length);
 
   return (
     <List.Section title={title}>
-      {items?.map((episode) => <EpisodeListItem key={episode.id} episode={episode} />)}
+      {items?.map((episode) => (
+        <EpisodeListItem key={episode.id} episode={episode} />
+      ))}
     </List.Section>
   );
 }
