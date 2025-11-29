f0701f30f0406fbc9f581cfe195d0a43dfebf301
Robert Sefer <rsefer@gmail.com>
2025-11-29 09:29:43 -0600

    Fixes spotify session issue

--- extensions/spotify-player/src/helpers/withSpotifyClient.tsx
+++ extensions/spotify-player/src/helpers/withSpotifyClient.tsx
@@ -2,6 +2,7 @@ import { provider } from "../api/oauth";
 import * as api from "../helpers/spotify.api";
 import nodeFetch from "node-fetch";
 import { withAccessToken } from "@raycast/utils";
+import { showHUD } from "@raycast/api";
 
 export let spotifyClient: typeof api | undefined;
 
@@ -31,16 +32,48 @@ export function getSpotifyClient() {
 }
 
 export async function setSpotifyClient() {
-  const accessToken = await provider.authorize();
 
-  // Send this header with each request
-  api.defaults.headers = {
-    Authorization: `Bearer ${accessToken}`,
-  };
+	// retry fix from https://github.com/raycast/extensions/issues/9988#issuecomment-2403142878
 
-  // Use this instead of the global fetch
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  api.defaults.fetch = nodeFetch as any;
+	const isConnected = await checkNetworkConnectivity();
+  if (!isConnected) {
+    await showHUD("Network is not available. Please check your VPN connection.");
+    return;
+  }
 
-  spotifyClient = api;
+	let retries = 3;
+  while (retries > 0) {
+    try {
+			const accessToken = await provider.authorize();
+
+			// Send this header with each request
+			api.defaults.headers = {
+				Authorization: `Bearer ${accessToken}`,
+			};
+
+			// Use this instead of the global fetch
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			api.defaults.fetch = nodeFetch as any;
+
+			spotifyClient = api;
+			return;
+		} catch (error) {
+			console.error("Error during authentication:", error);
+			retries--;
+			if (retries === 0) {
+				await showHUD("Failed to authenticate with Spotify. Please try again.");
+				throw error;
+			}
+			await new Promise(resolve => setTimeout(resolve, 5000)); // Wait 5 seconds before retrying
+		}
+	}
+}
+
+async function checkNetworkConnectivity() {
+  try {
+    await fetch('https://api.spotify.com', { method: 'HEAD', mode: 'no-cors' });
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
